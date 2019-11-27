567bf5f8164d42da7016e2c3adbedc9090e0ee51
Whiteheadcrab <lettersorstudy@gmail.com>
2019-11-27 20:18:54 +0100

    Version 2.4.1 Located and solved problem which makes not starting application

--- springframework/src/main/java/whiteheadcrab/java/spring/springtraining/model/Publisher.java
+++ springframework/src/main/java/whiteheadcrab/java/spring/springtraining/model/Publisher.java
@@ -10,16 +10,16 @@ public class Publisher
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id;
+    private Long id;
     private String name;
     private String address;
 
-    public String getId()
+    public Long getId()
     {
         return id;
     }
 
-    public void setId(String id)
+    public void setId(Long id)
     {
         this.id = id;
     }
